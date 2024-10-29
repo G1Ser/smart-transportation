30e2dd8369a2f6304e240b5bb207286f63c7a7d0
Chauncey <chauncey1129@163.com>
2024-10-30 00:26:30 +0800

    缺少验证码 form验证样式需要调整

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,50 @@
 <template>
   <div class="login">
     <h1 class="title">光谷智慧交通系统</h1>
-    <div class="login-form">
-      <el-form :label-position="labelPosition" label-width="auto">
-        <el-form-item label="用户名：">
-          <el-input />
-        </el-form-item>
-        <el-form-item label="密码：">
-          <el-input />
-        </el-form-item>
-      </el-form>
+    <div class="content">
+      <div class="login-form">
+        <el-form ref="ruleFormRef" :label-position="labelPosition" label-width="auto" :model="ruleForm" :rules="rules">
+          <el-form-item label="用户名：" prop="username">
+            <el-input v-model="ruleForm.username" placeholder="请输入用户名" />
+          </el-form-item>
+          <el-form-item label="密码：" prop="password">
+            <el-input v-model="ruleForm.password" type="password" show-password placeholder="请输入用户密码" />
+          </el-form-item>
+          <el-form-item label="验证码：" prop="verificationCode">
+            <el-input v-model="ruleForm.verificationCode" placeholder="请输入验证码" />
+          </el-form-item>
+        </el-form>
+        <el-checkbox label="30天免登录" size='large' />
+        <div class="btns">
+          <el-button type="primary" round>登录</el-button>
+          <el-button type="info" round>忘记密码</el-button>
+          <el-button round>注册</el-button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
-import type { FormProps } from "element-plus";
-const labelPosition = ref<FormProps['labelPosition']>('left')
+import { ref, reactive } from "vue";
+import type { FormInstance, FormProps, FormRules } from "element-plus";
+const labelPosition = ref<FormProps['labelPosition']>('left');
+const ruleFormRef = ref<FormInstance>()
+const ruleForm = reactive({
+  username: '',
+  password: '',
+  verificationCode: ''
+})
+interface RuleForm {
+  username: string
+  password: string
+  verificationCode: string
+}
+const rules = reactive<FormRules<RuleForm>>({
+  username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+  password: [{ required: true, message: '请输入用户密码', trigger: 'blur' }],
+  verificationCode: [{ required: true, message: '请输入验证码', trigger: 'blur' }],
+})
 </script>
 
 <style lang="scss" scoped>
@@ -26,23 +53,65 @@ const labelPosition = ref<FormProps['labelPosition']>('left')
   width: 1920px;
   height: 1080px;
   text-align: center;
-  background: linear-gradient(
-      rgba(183, 194, 194, 0.3),
-      rgba(183, 194, 194, 0.7)
-    ),
+  background: linear-gradient(rgba(183, 194, 194, 0.3),
+      rgba(183, 194, 194, 0.7)),
     no-repeat center/cover url("@/assets/img/background.jpg");
+
   .title {
-    transform: translateY(150px);
+    transform: translateY(200px);
   }
-  .login-form{
+
+  .content {
     display: flex;
     justify-content: center;
-    transform: translateY(250px);
-    .el-form{
-      width: 500px;
-      padding: 20px 20px 2px 20px;
-      border-radius: 6px;
-      border: 1px solid;
+
+    .login-form {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      transform: translateY(350px);
+      position: relative;
+      width: 550px;
+      height: 300px;
+      padding: 20px 20px 20px 20px;
+      border-radius: 15px;
+      backdrop-filter: blur(2px);
+      background-color: rgba(200, 200, 200, 0.2);
+      box-shadow: 3px 3px 15px rgba(0, 0, 0, 0.3);
+
+      .el-form {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        width: 500px;
+      }
+
+      :deep(.el-form-item__label) {
+        color: black;
+        font-size: 18px;
+      }
+
+      .el-checkbox {
+        position: absolute;
+        left: 30px;
+        bottom: 5px;
+        color: black;
+      }
+
+      .btns {
+        display: flex;
+        justify-content: flex-end;
+        gap: 20px;
+        position: absolute;
+        right: 20px;
+        bottom: 10px;
+
+        .el-button {
+          width: 100px;
+          color: black;
+          font-size: 18px;
+        }
+      }
     }
   }
 }
