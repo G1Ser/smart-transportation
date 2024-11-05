45c00055215df9a0f47e06389c30bc2c58205200
chauncey <chauncey1129@163.com>
2024-11-05 09:15:31 +0800

    添加一些配置文件

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  server: {
+    host: true,
+    proxy: {
+      "/": {
+        target: "http://192.168.22.229:8080",
+        changeOrigin: true,
+      },
+    },
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
