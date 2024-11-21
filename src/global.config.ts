d2d1b811bb9a6021a51b344bae335765fe9c971f
Chauncey <chauncey1129@163.com>
2024-11-22 00:42:59 +0800

    加载mapbox 调整css

--- src/global.config.ts
+++ src/global.config.ts
@@ -1,3 +1,4 @@
 export default {
   whiteListApi: ["/user/register", "/user/login", "/user/forgetPwd"],
+  mapboxToken:"pk.eyJ1IjoicGttbmk3YXZsbSIsImEiOiJjbTJqMDY5MWYwMGYyMnJvdmYxaDI4ODR3In0.UZbrQIY-5-GpkhJr04dQyw"
 };
--- src/views/MapBoard.vue
+++ src/views/MapBoard.vue
@@ -1,11 +1,29 @@
 <template>
-    <div>
-        this is mapboard page!
-    </div>
+    <div id="map" />
 </template>
 
 <script lang="ts" setup>
+import mapboxgl from 'mapbox-gl'
+import { onMounted } from 'vue';
+import globalConfig from "@/global.config";
 
+onMounted(() => {
+    mapboxgl.accessToken = globalConfig.mapboxToken;
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [0, 0],
+        zoom: 1
+    });
+    map.on("style.load", () => {
+        map.setFog({
+            "color": "#000022",
+            "high-color": "#000022",
+            "space-color": "#4169e1"
+        })
+    })
+})
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
